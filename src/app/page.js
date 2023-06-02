0813b9100478ed8472a410e75d5e2d2271ad3502
mansithanki <thankimansi22@gmail.com>
2023-06-02 18:12:21 -0400

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-200">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Mansi's Page!</h1>
+      <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page!</h1>
       <div className="flex flex-col items-center space-y-4">
         <Link href="/userslist" className="text-blue-500 hover:underline">
           Click here to check out Users Page
